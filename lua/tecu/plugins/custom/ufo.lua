fe33d3a694b3d705b8a70e7859691aa2f3bbd9e6
Tecu23 <andrei.teculescu@gmail.com>
2024-04-30 08:34:57 +0300

    fix folding and add handler for folds

--- lua/tecu/plugins/custom/ufo.lua
+++ lua/tecu/plugins/custom/ufo.lua
@@ -1,6 +1,7 @@
 local M = {
     "kevinhwang91/nvim-ufo",
-    event = "User FilePost",
+    event = { "BufReadPost", "BufNewFile" },
+    lazy = true,
     init = function()
         vim.o.foldcolumn = "1"
         vim.o.foldlevel = 99
@@ -16,21 +17,76 @@ local M = {
                 local builtin = require("statuscol.builtin")
                 require("statuscol").setup({
                     relculright = true,
+                    ft_ignore = {},
                     segments = {
-                        { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
-                        { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-                        { text = { "%s" }, click = "v:lua.ScSa" },
+                        -- Diagnostics
+                        {
+                            sign = { name = { "Diagnostic" }, maxwidth = 2, auto = false },
+                            click = "v:lua.ScSa",
+                        },
+
+                        -- Folds
+                        {
+                            text = { builtin.foldfunc },
+                        },
+                        -- Relative Line Numbers
+                        {
+                            text = { builtin.lnumfunc },
+                            condition = { true, builtin.not_empty },
+                        },
+
+                        -- Gitsigns
+                        {
+                            sign = {
+                                namespace = { "gitsigns" },
+                                maxwidth = 1,
+                                colwidth = 1,
+                                wrap = true,
+                            },
+                        },
                     },
                 })
             end,
         },
     },
 }
-
 function M.config()
     require("ufo").setup({
+        open_fold_hl_timeout = 150,
+        enable_get_fold_virt_text = true,
+        close_fold_kinds_for_ft = {
+            default = { "imports", "comment" },
+        },
+        preview = {
+            win_config = {
+                border = { "", "─", "", "", "", "─", "", "" },
+                winhighlight = "Normal:Folded",
+                winblend = 0,
+            },
+            mappings = {
+                scrollU = "<C-u>",
+                scrollD = "<C-d>",
+                jumpTop = "[",
+                jumpBot = "]",
+            },
+        },
+        provider_fold_virt_text_handler = function(text, lnum, endLnum, width)
+            local suffix = "  "
+            local lines = ("[%d lines] "):format(endLnum - lnum)
+
+            local cur_width = 0
+            for _, section in ipairs(text) do
+                cur_width = cur_width + vim.fn.strdisplaywidth(section[1])
+            end
+
+            suffix = suffix .. (" "):rep(width - cur_width - vim.fn.strdisplaywidth(lines) - 3)
+
+            table.insert(text, { suffix, "Comment" })
+            table.insert(text, { lines, "Todo" })
+            return text
+        end,
         provider_selector = function(_, _, _)
-            return { "treesitter", "indent" }
+            return { "lsp", "indent" }
         end,
     })
 
